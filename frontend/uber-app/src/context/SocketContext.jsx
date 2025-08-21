fa78fac23c13de243ee57c35ddba212b94f1e02e
vaibhav123-tech <124490668+vaibhav123-tech@users.noreply.github.com>
2025-08-22 03:23:42 +0530

    change

--- frontend/uber-app/src/context/SocketContext.jsx
+++ frontend/uber-app/src/context/SocketContext.jsx
@@ -8,8 +8,9 @@ import { io } from 'socket.io-client';
 export const SocketContext = createContext();
 
 const socket = io(import.meta.env.VITE_BASE_URL, {
-  transports: ["websocket"], // helps avoid polling issues
-});// Replace with your server URL
+  transports: ["websocket"],
+  withCredentials: true
+});
 
  export const SocketProvider = ({ children }) => {
     useEffect(() => {
